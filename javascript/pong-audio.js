d15824a411ba4f9ca8c3f0a6e1892216a3dda8aa
Attackonkilljoy <133689645+Attackonkilljoy@users.noreply.github.com>
2023-05-25 10:29:54 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,13 +49,13 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("ding-126626.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("silence.mp3");
+export var scoreSound = new soundFile("ding-126626.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("MackaParkHydrophonefountain.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = 5; //turn down volume
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
